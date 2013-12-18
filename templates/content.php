c4c8beded4c6d34065a2fbf686b7cb324a44ba10
Sam Gaus <git@gaus.ie>
2013-12-17 16:05:22 -0800

    Cleaned up header spacing and comments

--- templates/content.php
+++ templates/content.php
@@ -11,12 +11,3 @@
     <p><?php the_title(); ?></p>  
   </span>  
 </article>
-<!--
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
--->
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,7 +1,5 @@
-<div class="page-header">
-<?php if(!is_home()){ ?>
+<div class="page-header"><?php if(!is_home()){ ?>
   <h1>
     <?php echo roots_title(); ?>
   </h1>
-<?php } ?>
-</div>
+<?php } ?></div>
