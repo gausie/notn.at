65d8773f1bededb18cf9af007281d7c30ed3d7b0
Sam Gaus <git@gaus.ie>
2013-12-19 19:44:10 -0800

    Left something silly in...

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,6 @@
     <div class="col-lg-12">
       <a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
       <nav class="nav-main" role="navigation">
-        WOW
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills'));
