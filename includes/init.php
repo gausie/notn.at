9c1612b43474c318bcacb03f14977a23a5110c59
Sam Gaus <git@gaus.ie>
2013-12-19 17:49:16 -0800

    Moved favicons from head.php to the wp_head action, and then included in admin_head so the admin area has favicons too

--- includes/init.php
+++ includes/init.php
@@ -20,13 +20,36 @@ add_action('wp_enqueue_scripts', function() {
 });
 
 /**
+ * Favicons
+ */
+ 
+add_action('wp_head', 'notnat_favicons');
+add_action('admin_head', 'notnat_favicons');
+function notnat_favicons(){
+?>
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/favicon.ico">
+  <link rel="icon" type="image/png" sizes="64x64" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/favicon.png">
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon.png">
+  <link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon-72x72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon-76x76-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon-114x114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon-120x120-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon-144x144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-touch-icon-152x152-precomposed.png">
+  <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/windows-tile-144x144.png">
+  <meta name="msapplication-TileColor" content="#515151">
+<?php
+}
+
+/**
  * Add support for custom header
  */
-function notnat_header_image() {
-  echo "<img src=\"" . get_header_image() . "\" />";
-}
+
 add_theme_support('custom-header', array(
-  'admin-preview-callback' => 'notnat_header_image',
+  'admin-preview-callback' => function() {
+    echo "<img src=\"" . get_header_image() . "\" />";
+  }
 ));
 
 /**
