0b25559b0742c8cceeaa02995c3b4f0297a964ac
Sam Gaus <git@gaus.ie>
2013-12-21 19:13:47 -0800

    Fixed a misunderstanding I had of the relabelling

--- includes/portfolio-post-type.php
+++ includes/portfolio-post-type.php
@@ -15,21 +15,49 @@ add_action('pre_get_posts', function($query) {
 });
 
 /**
- * Change portfolio category slug
+ * Change portfolio language
  */
 add_filter('portfolioposttype_category_args', function($args){  
   $labels = array(
-      'name'               => __( 'Portfolio', 'portfolioposttype' ),
-      'singular_name'      => __( 'Piece', 'portfolioposttype' ),
-      'add_new'            => __( 'Add New Piece', 'portfolioposttype' ),
-      'add_new_item'       => __( 'Add New Piece', 'portfolioposttype' ),
-      'edit_item'          => __( 'Edit Piece', 'portfolioposttype' ),
-      'new_item'           => __( 'Add New Piece', 'portfolioposttype' ),
-      'view_item'          => __( 'View Piece', 'portfolioposttype' ),
-      'search_items'       => __( 'Search Portfolio', 'portfolioposttype' ),
-      'not_found'          => __( 'No pieces found', 'portfolioposttype' ),
-      'not_found_in_trash' => __( 'No pieces found in trash', 'portfolioposttype' ),
+      'name'               => __( 'Portfolio', 'portfolio-post-type' ),
+      'singular_name'      => __( 'Piece', 'portfolio-post-type' ),
+      'add_new'            => __( 'Add New Piece', 'portfolio-post-type' ),
+      'add_new_item'       => __( 'Add New Piece', 'portfolio-post-type' ),
+      'edit_item'          => __( 'Edit Piece', 'portfolio-post-type' ),
+      'new_item'           => __( 'Add New Piece', 'portfolio-post-type' ),
+      'view_item'          => __( 'View Piece', 'portfolio-post-type' ),
+      'search_items'       => __( 'Search Portfolio', 'portfolio-post-type' ),
+      'not_found'          => __( 'No pieces found', 'portfolio-post-type' ),
+      'not_found_in_trash' => __( 'No pieces found in trash', 'portfolio-post-type' ),
   );
+  $args['labels'] = $labels;
+
+  return $args;
+});
+
+/**
+ * Change portfolio category language and slug
+ */
+add_filter('portfolioposttype_category_args', function($args){  
+  $labels = array(
+    'name'                       => __( 'Portfolio Media', 'portfolio-post-type' ),
+    'singular_name'              => __( 'Portfolio Medium', 'portfolio-post-type' ),
+    'menu_name'                  => __( 'Portfolio Media', 'portfolio-post-type' ),
+    'edit_item'                  => __( 'Edit Portfolio Medium', 'portfolio-post-type' ),
+    'update_item'                => __( 'Update Portfolio Medium', 'portfolio-post-type' ),
+    'add_new_item'               => __( 'Add New Portfolio Medium', 'portfolio-post-type' ),
+    'new_item_name'              => __( 'New Portfolio Medium Name', 'portfolio-post-type' ),
+    'parent_item'                => __( 'Parent Portfolio Medium', 'portfolio-post-type' ),
+    'parent_item_colon'          => __( 'Parent Portfolio Medium:', 'portfolio-post-type' ),
+    'all_items'                  => __( 'All Portfolio Media', 'portfolio-post-type' ),
+    'search_items'               => __( 'Search Portfolio Media', 'portfolio-post-type' ),
+    'popular_items'              => __( 'Popular Portfolio Media', 'portfolio-post-type' ),
+    'separate_items_with_commas' => __( 'Separate portfolio media with commas', 'portfolio-post-type' ),
+    'add_or_remove_items'        => __( 'Add or remove portfolio media', 'portfolio-post-type' ),
+    'choose_from_most_used'      => __( 'Choose from the most used portfolio media', 'portfolio-post-type' ),
+    'not_found'                  => __( 'No portfolio media found.', 'portfolio-post-type' ),
+  );
+
   $args['labels'] = $labels;
 
   $args['rewrite'] = array(
