b8dd8be8dead699e36c7103b1b10c0daabc52236
Sam Gaus <git@gaus.ie>
2014-08-04 22:47:02 +0100

    News page shows blog posts

---
+++ page-news.php
@@ -0,0 +1,36 @@
+<?php 
+// the query
+
+$args = array(
+  'post_type' => 'post'
+);
+
+$the_query = new WP_Query( $args ); ?>
+
+<?php if (!$the_query->have_posts()) : ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'roots'); ?>
+  </div>
+  <?php get_search_form(); ?>
+<?php endif; ?>
+
+<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <?php get_template_part('templates/entry-meta'); ?>
+    </header>
+    <div class="entry-summary">
+      <?php the_excerpt(); ?>
+    </div>
+  </article>
+<?php endwhile; ?>
+
+<?php if ($wp_query->max_num_pages > 1) : ?>
+  <nav class="post-nav">
+    <ul class="pager">
+      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+    </ul>
+  </nav>
+<?php endif; ?>
